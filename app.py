7e2d677c96ab86c771ca044149b10b12260654e6
Pavithra-1307 <91123171+Pavithra-1307@users.noreply.github.com>
2022-09-01 15:10:39 +0530

    Add files via upload

--- app.py
+++ app.py
@@ -1,59 +1,40 @@
 from flask import Flask, request, redirect, render_template
-# """"""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""
-# ☛ Req -4 : Import db, Employee from models.py module
-# """"""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""
-
-# """"""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""
-# ☛ Req -5 : Create app object from Flask
-# """"""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""
-
-# """"""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""
-# ☛ Req -6 : Create database name called employee_project.db
-# """"""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""
-app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///<db_name>'
+from models import mydb,Employee
+app = Flask(__name__)
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///employee_project.mydb'
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
-
-# binding the instance to a specific Flask application
-db.init_app(app)
+mydb.init_app(app)
 
 
 @app.before_first_request
 def create_table():
-    db.create_all()
+    mydb.create_all()
 
 
 @app.route('/')
-# """"""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""
-# ☛ Req -7 : Create a method/function called landing_page() which should return a string 'You have landed EEE project-2022'
-# """"""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""
-def hello_world():  # put application's code here
-    return 'Hello World!'
-
+def index():
+    all_data = Employee.query.all()
+    return render_template("index.html", employees=all_data)
 
 @app.route('/data/create', methods=['GET', 'POST'])
 def create():
     if request.method == 'GET':
-        # """"""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""
-        # ☛ Req -8 : Above if condition should render a template file called createpage.html
-        # """"""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""
-
+        return render_template('createpage.html')
     if request.method == 'POST':
         employee_id = request.form['employee_id']
         name = request.form['name']
         age = request.form['age']
         position = request.form['position']
-        employee = Employee(employee_id=employee_id, name=name, age=age, position=position)
-        db.session.add(employee)
-        db.session.commit()
-        # """"""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""
-        # ☛ Req -9 : Above if condition should redirect to URI '/data'
-        # """"""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""
+        email = request.form['email']
+        employee = Employee(employee_id=employee_id, name=name, age=age, position=position,email=email)
+        mydb.session.add(employee)
+        mydb.session.commit()
+        return redirect('/data')
+
 
 @app.route('/data')
 def get_all_employees():
-    # """"""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""
-    # ☛ Req -10 : Fetch/Query all the rows and assign it to a variable called "employees"
-    # """"""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""
+    employees = Employee.query.all()
     return render_template('datalist.html', employees=employees)
 
 
@@ -62,27 +43,25 @@ def get_employee(id):
     employee = Employee.query.filter_by(employee_id=id).first()
     if employee:
         return render_template('data.html', employee=employee)
-    return <text_will_go_here>
-    # """""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""
-    # ☛ Req -11 : if given employee id was not found it should return a text called "Submitted id 123 is not found in employee database
-    # """""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""
+    return f"Submitted id 123 is not found in employee database"
+
 
 @app.route('/data/<int:id>/update', methods=['GET', 'POST'])
 def update(id):
     employee = Employee.query.filter_by(employee_id=id).first()
     if request.method == 'POST':
         if employee:
-            db.session.delete(employee)
-            db.session.commit()
+            mydb.session.delete(employee)
+            mydb.session.commit()
             name = request.form['name']
             age = request.form['age']
             position = request.form['position']
-            employee = Employee(employee_id=id, name=name, age=age, position=position)
-            db.session.add(employee)
-            db.session.commit()
-            return redirect(f'/data/{id}')
+            email = request.form['email']
+            employee = Employee(employee_id=id, name=name, age=age, position=position, email=email)
+            mydb.session.add(employee)
+            mydb.session.commit()
+            return redirect('/data')
         return f"Employee with id = {id} Does nit exist"
-
     return render_template('update.html', employee=employee)
 
 
@@ -91,15 +70,10 @@ def delete(id):
     employee = Employee.query.filter_by(employee_id=id).first()
     if request.method == 'POST':
         if employee:
-            # """""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""
-            # ☛ Req -12 : Ensure given record (1) get deleted and (2) Committed
-            # """""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""
+            mydb.session.delete(employee)
+            mydb.session.commit()
             return redirect('/data')
-
     return render_template('delete.html')
 
-
 if __name__ == '__main__':
-    # """""""""""""""""""""""""""""""""""
-    # ☛ Req -13 : Run app in debug mode
-    # """""""""""""""""""""""""""""""""""
+    app.run(debug=True)
